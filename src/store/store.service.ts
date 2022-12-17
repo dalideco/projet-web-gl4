2a78fa697facb770495b393fc180400c11be36e8
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2022-12-18 00:17:32 +0100

    store service

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Store } from 'entities/store.entity';
+import { Repository } from 'typeorm';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoreService {
+
+  constructor(
+    @InjectRepository(Store)
+    private readonly storeRepository: Repository<Store>,
+  ) {}
+
   create(createStoreDto: CreateStoreDto) {
-    return 'This action adds a new store';
+    const store = this.storeRepository.create(createStoreDto);
+    return this.storeRepository.save(store);
   }
 
   findAll() {
-    return `This action returns all store`;
+    return this.storeRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} store`;
+    return this.storeRepository.findOneBy({id:id});
   }
 
   update(id: number, updateStoreDto: UpdateStoreDto) {
@@ -21,6 +31,6 @@ export class StoreService {
   }
 
   remove(id: number) {
-    return `This action removes a #${id} store`;
+    return this.storeRepository.softDelete(id);
   }
 }
