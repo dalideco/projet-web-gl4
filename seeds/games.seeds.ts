d49ff16971852ac061beacf3c64cc9274701d6a5
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2023-01-15 12:30:57 +0100

    Update games.seeds.ts

--- seeds/games.seeds.ts
+++ seeds/games.seeds.ts
@@ -2,9 +2,9 @@ import { CreateGameDto } from 'src/game/dto/create-game.dto';
 
 export const games: CreateGameDto[] = [
   {
-    title: 'Red Dead Redemption',
+    title: 'Red Dead Redemption 2',
     description: 'Open world game! ',
-    image: '',
+    image: 'pic/RDR2.jpg',
   },
   {
     title: "Assassin's creend Unity",
@@ -31,4 +31,173 @@ export const games: CreateGameDto[] = [
     description: 'Open world game! ',
     image: '',
   },
+  {
+    title: 'ARK Survival Evolved',
+    description: 'Open world game! ',
+    image: 'pic/ARKSurvivalEvolved.jpg',
+  },
+  {
+    title: 'bloons td 6',
+    description: 'Open world game! ',
+    image: 'pic/bloons-td-6.jpg',
+  },
+  {
+    title: 'callisto protocol',
+    description: 'Open world game! ',
+    image: 'pic/callisto-protocol.jpg',
+  },
+  {
+    title: 'charons staircase',
+    description: 'Open world game! ',
+    image: 'pic/charons-staircase.jpg',
+  },
+  {
+    title: 'children of silentown',
+    description: 'Open world game! ',
+    image: 'pic/children of silentown.jpg',
+  },
+  {
+    title: 'Cities Skylines',
+    description: 'Open world game! ',
+    image: 'pic/CitiesSkylines.jpg',
+  },
+  {
+    title: 'edge of galaxy',
+    description: 'Open world game! ',
+    image: 'pic/edge-of-galaxy.png',
+  },
+  {
+    title: 'Grand Theft Auto v',
+    description: 'Open world game! ',
+    image: 'pic/GTAV.jpg',
+  },
+  {
+    title: 'high on life',
+    description: 'Open world game! ',
+    image: 'high-on-life.jpg',
+  },
+  {
+    title: 'ixion',
+    description: 'Open world game! ',
+    image: 'pic/ixion.jpg',
+  },
+  {
+    title: 'jedi survivor',
+    description: 'Open world game! ',
+    image: 'pic/jedi-survivor.jpg',
+  },
+  {
+    title: 'my friend peppa pig',
+    description: 'Open world game! ',
+    image: 'pic/my-friend-peppa-pig.jpg',
+  },
+  {
+    title: 'my little pony a maretime bay adventure',
+    description: 'Open world game! ',
+    image: 'pic/my-little-pony-a-maretime-bay-adventure.jpg',
+  },
+  {
+    title: 'new joe and mac caveman ninja',
+    description: 'Open world game! ',
+    image: 'pic/new-joe-and-mac-caveman-ninja.jpg',
+  },
+  {
+    title: 'nhra championship drag racing speed for all',
+    description: 'Open world game! ',
+    image: 'pic/nhra-championship-drag-racing--speed-for-all.jpg',
+  },
+  {
+    title: 'Panzer Corps 2',
+    description: 'Open world game! ',
+    image: 'pic/PanzerCorps2.jpg',
+  },
+  {
+    title: 'paw patrol mighty pups save adventure bay',
+    description: 'Open world game! ',
+    image: 'pic/paw-patrol-mighty-pups-save-adventure-bay.png',
+  },
+  {
+    title: 'RimWorld',
+    description: 'Open world game! ',
+    image: 'pic/RimWorld.jpg',
+  },
+  {
+    title: 'sentinel 3:homeworld',
+    description: 'Open world game! ',
+    image: 'pic/sentinel-3-homeworld.png',
+  },
+  {
+    title: 'shadow tactics',
+    description: 'Open world game! ',
+    image: 'pic/shadow-tactics.jpg',
+  },
+  {
+    title: 'shoulders of giants',
+    description: 'Open world game! ',
+    image: 'pic/shoulders-of-giants.jpg',
+  },
+  {
+    title: 'somerville',
+    description: 'Open world game! ',
+    image: 'pic/somerville.jpg',
+  },
+  {
+    title: 'STAR WARS BattlefrontII',
+    description: 'Open world game! ',
+    image: 'pic/STAR-WARS-BattlefrontII.jpg',
+  },
+  {
+    title: 'tails of iron',
+    description: 'Open world game! ',
+    image: 'pic/tails-of-iron.png',
+  },
+  {
+    title: 'the galactic junkers',
+    description: 'Open world game! ',
+    image: 'pic/bloons-td-6.png',
+  },
+  {
+    title: 'the last of us part1',
+    description: 'Open world game! ',
+    image: 'pic/the-last-of-us-p1.jpg',
+  },
+  {
+    title: 'The Long Dark',
+    description: 'Open world game! ',
+    image: 'pic/The-Long-Dark.jpg',
+  },
+  {
+    title: 'the shapeshifting detective',
+    description: 'Open world game! ',
+    image: 'pic/the-shapeshifting-detective.jpg',
+  },
+  {
+    title: 'tortuga',
+    description: 'Open world game! ',
+    image: 'pic/tortuga.jpg',
+  },
+  {
+    title: 'Tropico6',
+    description: 'Open world game! ',
+    image: 'pic/Tropico6',
+  },
+  {
+    title: 'trudograd',
+    description: 'Open world game! ',
+    image: 'pic/trudograd.png',
+  },
+  {
+    title: 'true hate',
+    description: 'Open world game! ',
+    image: 'pic/true-hate.png',
+  },
+  {
+    title: 'valorant',
+    description: 'Open world game! ',
+    image: 'pic/valorant.jpg',
+  },
+  
+
+
+  
 ];
