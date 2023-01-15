9852cb59a4f4100f7efe6f8fecc2985fa1529c6e
dalideco <dalissj1@gmail.com>
2023-01-15 13:53:12 +0100

    setting games and stores urls when getting item

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from '@nestjs/common';
-import { ItemService } from './item.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { Public } from 'decorators/public.decorator';
+import { stores } from 'seeds/stores.seeds';
+import { GameService } from 'src/game/game.service';
+import { StoreService } from 'src/store/store.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
-import { Roles } from 'decorators/roles.decoroator';
-import Role from 'models/role.enum';
-import { Public } from 'decorators/public.decorator';
 import { EditItemGuard } from './edit-guard/edit-item.guard';
+import { ItemService } from './item.service';
 
 @Controller('item')
 export class ItemController {
-  constructor(private readonly itemService: ItemService) {}
+  constructor(
+    private readonly itemService: ItemService,
+    private readonly gameService: GameService,
+    private readonly storeService: StoreService,
+  ) {}
 
   @Post()
   create(@Body() createItemDto: CreateItemDto, @Request() req) {
@@ -20,13 +35,35 @@ export class ItemController {
   @Public()
   @Get()
   findAll() {
-    return this.itemService.findAll();
+    return this.itemService.findAll().then((items) =>
+      items.map((item) => ({
+        ...item,
+        games: item.games.map((game) => ({
+          ...game,
+          image: this.gameService.transformUrl(game.image),
+        })),
+        store: {
+          ...item.store,
+          image: this.storeService.transformUrl(item.store.image),
+        },
+      })),
+    );
   }
 
   @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.itemService.findOne(+id);
+    return this.itemService.findOne(+id).then((item) => ({
+      ...item,
+      games: item.games.map((game) => ({
+        ...game,
+        image: this.gameService.transformUrl(game.image),
+      })),
+      store: {
+        ...item.store,
+        image: this.storeService.transformUrl(item.store.image),
+      },
+    }));
   }
 
   //TODO: only for a specific user
@@ -45,15 +82,15 @@ export class ItemController {
 
   @UseGuards(EditItemGuard)
   @Patch(':id/delete-game/:gameid')
-  removeGame(@Param('id') id: string , @Param('gameid') gameId: string ){
-    console.log("removing game",id, gameId)
-    return this.itemService.removeGame(+id,+gameId);
+  removeGame(@Param('id') id: string, @Param('gameid') gameId: string) {
+    console.log('removing game', id, gameId);
+    return this.itemService.removeGame(+id, +gameId);
   }
 
   @UseGuards(EditItemGuard)
   @Patch(':id/add-game/:gameid')
-  addGame(@Param('id') id: string , @Param('gameid') gameId: string ){
-    console.log("adding game",id, gameId)
-    return this.itemService.addGame(+id,+gameId);
+  addGame(@Param('id') id: string, @Param('gameid') gameId: string) {
+    console.log('adding game', id, gameId);
+    return this.itemService.addGame(+id, +gameId);
   }
 }
