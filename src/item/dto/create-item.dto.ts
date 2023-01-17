8d55861fbdfa81c51e43d1ccbad6eb2ae31d2098
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2023-01-17 22:02:14 +0100

    item filter

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,6 +1,9 @@
 import { IsNumber, IsString } from 'class-validator';
 import ItemType from 'models/ItemType.enum';
 export class CreateItemDto {
+  @IsString()
+  name: string;
+
   @IsNumber()
   price: number;
 
